eed3348a5c3a7ecad8224618adbaa39bb19ccc37
Mark Ayles <marka2049@gmail.com>
2022-04-19 23:26:14 -0400

    initial commit

---
+++ src/main/java/dev/ayles/casestudy/MarkAylesTekCaseStudyApplication.java
@@ -0,0 +1,13 @@
+package dev.ayles.casestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarkAylesTekCaseStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarkAylesTekCaseStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/ayles/casestudy/MarkAylesTekCaseStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ayles.casestudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarkAylesTekCaseStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
